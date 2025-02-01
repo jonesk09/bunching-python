128eb1697021895e9594518428dea765e0035a46
jonesk09 <89207072+jonesk09@users.noreply.github.com>
2025-01-31 20:16:56 -0500

    Create 10_globals.py
    conversion of R-bunching "globals" function to python using claude.ai

---
+++ 10_globals.py
@@ -0,0 +1,15 @@
+# Define constants used across functions
+GLOBAL_VARS = {
+    'bin',
+    'freq_orig',
+    'cf', 
+    'actual',
+    'z',
+    'bunch_region',
+    'key',
+    'value',
+    'cf_graph'
+}
+
+# If you need to reference these variables in your functions, you can use:
+# from your_module import GLOBAL_VARS
